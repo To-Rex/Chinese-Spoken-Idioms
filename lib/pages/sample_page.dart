9bcf091eff1fca48a2dac6b761b61858fedec524
to-rex <torex.amaki@gmail.com>
2024-02-26 21:37:04 +0500

    first commit

--- lib/pages/sample_page.dart
+++ lib/pages/sample_page.dart
@@ -1,9 +1,6 @@
-import 'package:chinese_spoken_idioms/pages/detail_page.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-
 import '../controllers/get_controller.dart';
-import 'import_page.dart';
 
 class SamplePage extends StatelessWidget {
   SamplePage({super.key});

74fbf37b59597d6255a3e37863df6195365a710f
to-rex <torex.amaki@gmail.com>
2024-02-28 02:18:22 +0500

    first commit

--- lib/pages/detail_page.dart
+++ lib/pages/detail_page.dart
@@ -11,7 +11,16 @@ class DetailPage extends StatelessWidget {
   var comment;
   var reminder;
   var examples;
-  DetailPage({super.key, required this.id, required this.character, required this.character2, required this.pinyin, required this.comment, required this.reminder, required this.examples});
+
+  DetailPage(
+      {super.key,
+      required this.id,
+      required this.character,
+      required this.character2,
+      required this.pinyin,
+      required this.comment,
+      required this.reminder,
+      required this.examples});
 
   final GetController _getController = Get.put(GetController());
 
@@ -26,11 +35,15 @@ class DetailPage extends StatelessWidget {
               Navigator.pop(context);
             },
           ),
-          title: Text('Detail Page', style: TextStyle(color: Theme.of(context).colorScheme.onBackground)),
+          title: Text('Detail Page',
+              style:
+                  TextStyle(color: Theme.of(context).colorScheme.onBackground)),
           actions: [
             IconButton(
               icon: Icon(
-                _getController.checkCollection(id) ? Icons.bookmark : Icons.bookmark_border,
+                _getController.checkCollection(id)
+                    ? Icons.bookmark
+                    : Icons.bookmark_border,
                 color: Theme.of(context).colorScheme.error,
               ),
               onPressed: () {
@@ -44,14 +57,15 @@ class DetailPage extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.start,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              SizedBox(height: _getController.height * 0.05, width: _getController.width * 1),
+              SizedBox(
+                  height: _getController.height * 0.05,
+                  width: _getController.width * 1),
               Text(character,
                   style: TextStyle(
                     fontSize: _getController.width * 0.13,
                     color: Theme.of(context).colorScheme.onSurface,
                     fontWeight: FontWeight.w900,
-                  )
-              ),
+                  )),
               Container(
                 width: _getController.width * 0.3,
                 height: 2,
@@ -67,42 +81,38 @@ class DetailPage extends StatelessWidget {
                     fontSize: _getController.width * 0.05,
                     color: Colors.green,
                     fontWeight: FontWeight.w900,
-                  )
-              ),
-              Text(pinyin, style: TextStyle(fontSize: _getController.width * 0.05)),
+                  )),
+              Text(pinyin,
+                  style: TextStyle(fontSize: _getController.width * 0.05)),
               SizedBox(height: _getController.height * 0.05),
-              Container(
-                width: _getController.width * 0.9,
-                padding: EdgeInsets.all(_getController.width * 0.03),
-                decoration: BoxDecoration(
-                  color: Theme.of(context).colorScheme.onSecondary,
-                  borderRadius: BorderRadius.circular(10),
+              if (comment != null)
+                Container(
+                  width: _getController.width * 0.9,
+                  padding: EdgeInsets.all(_getController.width * 0.03),
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).colorScheme.onSecondary,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Text('Comment: $comment', style: TextStyle(fontSize: _getController.width * 0.045)),
                 ),
-                child: Text('Comment: $comment', style: TextStyle(fontSize: _getController.width * 0.045)),
-              ),
-              Container(
-                  width: _getController.width * 0.9,
-                  margin: EdgeInsets.only(top: _getController.height * 0.03),
-                  padding: EdgeInsets.all(_getController.width * 0.03),
-                  decoration: BoxDecoration(
-                    color: Theme.of(context).colorScheme.onSecondary,
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: Text('Reminder: $reminder', style: TextStyle(fontSize: _getController.width * 0.045))
-              ),
-              Container(
-                  width: _getController.width * 0.9,
-                  margin: EdgeInsets.only(top: _getController.height * 0.03,bottom: _getController.height * 0.03),
-                  padding: EdgeInsets.all(_getController.width * 0.03),
-                  decoration: BoxDecoration(
-                    color: Theme.of(context).colorScheme.onSecondary,
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: Text('Examples: $examples', style: TextStyle(fontSize: _getController.width * 0.045))
-              ),
+              if (reminder != null)
+                Container(
+                    width: _getController.width * 0.9,
+                    margin: EdgeInsets.only(top: _getController.height * 0.03),
+                    padding: EdgeInsets.all(_getController.width * 0.03),
+                    decoration: BoxDecoration(color: Theme.of(context).colorScheme.onSecondary, borderRadius: BorderRadius.circular(10),),
+                    child: Text('Reminder: $reminder', style: TextStyle(fontSize: _getController.width * 0.045))
+                ),
+              if (examples != null)
+                Container(
+                    width: _getController.width * 0.9,
+                    margin: EdgeInsets.only(top: _getController.height * 0.03, bottom: _getController.height * 0.03),
+                    padding: EdgeInsets.all(_getController.width * 0.03),
+                    decoration: BoxDecoration(color: Theme.of(context).colorScheme.onSecondary, borderRadius: BorderRadius.circular(10)),
+                    child: Text('Examples: $examples', style: TextStyle(fontSize: _getController.width * 0.045))
+                ),
             ],
           ),
-        )
-    ));
+        )));
   }
 }
